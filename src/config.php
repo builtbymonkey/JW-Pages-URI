54cd0bb98ecab4742cd154fc685497777fce091d
Jeremy Worboys <jw@jeremyworboys.com>
2013-10-11 10:09:45 +1100

    Bump version

--- src/config.php
+++ src/config.php
@@ -3,7 +3,7 @@
 if (!defined('JW_PAGES_URI_VERSION')) {
     define('JW_PAGES_URI_ID', 'jw_pages_uri');
     define('JW_PAGES_URI_NAME', 'JW Pages URI');
-    define('JW_PAGES_URI_VERSION', '1.0.0');
+    define('JW_PAGES_URI_VERSION', '1.1.0');
 }
 
 $config['name'] = JW_PAGES_URI_NAME;
